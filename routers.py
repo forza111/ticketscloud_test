d6e58a8466a07b56edc5c0c308f7ff241020c04f
forza111 <nikitos.ionkin@yandex.ru>
2021-11-24 14:54:43 +0300

    change function get_repo + create func. get_necessary_dataframe

--- routers.py
+++ routers.py
@@ -10,22 +10,29 @@ router = APIRouter(
 )
 
 @router.get('/{username}')
-async def get_user_repos(username: str):
-    return get_repo(username)
+async def get_user_repos(username: str, limit: int = 10):
+    return get_repo(username, limit)
 
-def get_repo(URL):
-    url = f"https://api.github.com/users/{URL}/repos"
+def get_repo(url, limit):
+    url = f"https://api.github.com/users/{url}/repos"
     dataframe = pd.DataFrame()
+    page = 1
     while True:
         try:
-            page = 1
             r = requests.get(url, auth=(USERNAME, TOKEN), params={"page":page})
             r.raise_for_status()
             assert len(r.json()) != 0
         except AssertionError:
             break
+        except Exception as e:
+            return e
         else:
             page += 1
             df = pd.json_normalize(r.json())
-            dataframe = pd.concat([dataframe, df[["id", "name"]]])
-    return dataframe[["id","name"]].to_dict(orient='records')
+            dataframe = pd.concat([dataframe, df[["id", "name", "stargazers_count", "html_url"]]])
+    return get_necessary_dataframe(dataframe, limit)
+
+def get_necessary_dataframe(dataframe, limit):
+    df = dataframe.rename(columns={'stargazers_count': "stars"})
+    return df.sort_values(by=['stars', "name"], ascending=[False, True])[:limit].to_dict(orient='records')
+
